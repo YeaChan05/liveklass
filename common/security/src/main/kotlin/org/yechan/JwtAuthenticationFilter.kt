80478411223177bec0fba512d72e2108644e34d4
qkenrdl05 <qkenrdl05@gmail.com>
2026-05-06 11:55:44 +0900

    feat(common): JwtAuthenticationFilter 추가

---
+++ common/security/src/main/kotlin/org/yechan/JwtAuthenticationFilter.kt
@@ -0,0 +1,39 @@
+package org.yechan
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.web.filter.OncePerRequestFilter
+
+class JwtAuthenticationFilter(
+    private val parser: TokenParser,
+    private val verifier: TokenVerifier,
+    private val authenticationEntryPoint: AuthenticationEntryPoint,
+) : OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain,
+    ) {
+        val token = parser.parse(request)
+        if (token == null) {
+            filterChain.doFilter(request, response)
+            return
+        }
+
+        try {
+            val authentication = verifier.verify(token)
+            val context = SecurityContextHolder.createEmptyContext()
+            context.authentication = authentication
+            SecurityContextHolder.setContext(context)
+            filterChain.doFilter(request, response)
+        } catch (ex: AuthenticationException) {
+            authenticationEntryPoint.commence(request, response, ex)
+        } finally {
+            SecurityContextHolder.clearContext()
+        }
+    }
+}
