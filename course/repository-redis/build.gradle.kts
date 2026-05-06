f8e184e0de766b04874fb40b8e991102494c4ae7
qkenrdl05 <qkenrdl05@gmail.com>
2026-05-07 07:57:41 +0900

    feat(build): 통합 테스트를 위한 TestContainers 추가

--- course/repository-redis/build.gradle.kts
+++ course/repository-redis/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
     implementation(project(":course:infrastructure"))
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    integrationTestImplementation("org.testcontainers:testcontainers:${libs.versions.testcontainers.get()}")
 }
