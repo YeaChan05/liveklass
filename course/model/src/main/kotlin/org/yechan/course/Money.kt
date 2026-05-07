309c17cfd4a6b48216ffd505340623dbc8e02d43
qkenrdl05 <qkenrdl05@gmail.com>
2026-05-07 12:54:49 +0900

    refactor(course): VO를 primitive type에 범용적이게 전환

--- course/model/src/main/kotlin/org/yechan/course/Money.kt
+++ course/model/src/main/kotlin/org/yechan/course/Money.kt
@@ -1,39 +1,76 @@
 package org.yechan.course
 
 import java.math.BigDecimal
+import java.math.RoundingMode
 
-data class Money(val amount: BigDecimal) {
+data class Money(
+    val amount: BigDecimal,
+) {
     init {
-        if (amount < BigDecimal.ZERO) throw IllegalArgumentException("금액은 음수일 수 없습니다.")
+        require(amount >= BigDecimal.ZERO) {
+            "금액은 음수일 수 없습니다."
+        }
     }
+
+    constructor(amount: Long) : this(BigDecimal.valueOf(amount))
+
+    constructor(amount: Int) : this(amount.toLong())
 
     operator fun plus(other: Money): Money = Money(this.amount + other.amount)
 
     operator fun minus(other: Money): Money {
         val result = this.amount - other.amount
-        if (result < BigDecimal.ZERO) throw IllegalArgumentException("결과 금액은 음수일 수 없습니다.")
+
+        require(result >= BigDecimal.ZERO) {
+            "결과 금액은 음수일 수 없습니다."
+        }
+
         return Money(result)
     }
 
     operator fun times(multiplier: Int): Money {
-        if (multiplier < 0) throw IllegalArgumentException("곱셈의 배수는 음수일 수 없습니다.")
-        return Money(this.amount * BigDecimal(multiplier))
+        require(multiplier >= 0) {
+            "곱셈의 배수는 음수일 수 없습니다."
+        }
+
+        return Money(this.amount.multiply(BigDecimal.valueOf(multiplier.toLong())))
+    }
+
+    operator fun times(multiplier: Long): Money {
+        require(multiplier >= 0) {
+            "곱셈의 배수는 음수일 수 없습니다."
+        }
+
+        return Money(this.amount.multiply(BigDecimal.valueOf(multiplier)))
     }
 
     operator fun div(divisor: Int): Money {
-        if (divisor <= 0) throw IllegalArgumentException("나눗셈의 분모는 양수여야 합니다.")
-        return Money(this.amount / BigDecimal(divisor))
+        require(divisor > 0) {
+            "나눗셈의 분모는 양수여야 합니다."
+        }
+
+        return Money(
+            this.amount.divide(
+                BigDecimal.valueOf(divisor.toLong()),
+                RoundingMode.DOWN,
+            ),
+        )
+    }
+
+    operator fun div(divisor: Long): Money {
+        require(divisor > 0) {
+            "나눗셈의 분모는 양수여야 합니다."
+        }
+
+        return Money(
+            this.amount.divide(
+                BigDecimal.valueOf(divisor),
+                RoundingMode.DOWN,
+            ),
+        )
     }
 
     override fun toString(): String = "Money(amount=$amount)"
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is Money) return false
-        return amount == other.amount
-    }
-
-    override fun hashCode(): Int = amount.hashCode()
 
     companion object {
         val ZERO = Money(BigDecimal.ZERO)
