45f8f24b7a2fea7397d5cec1b1ef1c5e3c529d35
qkenrdl05 <qkenrdl05@gmail.com>
2026-05-08 11:11:28 +0900

    feat(course): 강의 및 수강 관련 테이블 생성

--- course/schema/src/main/resources/db/changelog/course/initial_schema.sql
+++ course/schema/src/main/resources/db/changelog/course/initial_schema.sql
@@ -10,3 +10,47 @@ CREATE TABLE members (
     CONSTRAINT pk_members PRIMARY KEY (id),
     CONSTRAINT uk_members_email UNIQUE (email)
 );
+
+CREATE TABLE courses (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    creator_id BIGINT NOT NULL,
+
+    title VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    price DECIMAL(19,2) NOT NULL,
+
+    capacity INT NOT NULL,
+    seat_left_count INT NOT NULL,
+
+    period_start DATETIME(6) NOT NULL,
+    period_end DATETIME(6) NOT NULL,
+
+    status VARCHAR(20) NOT NULL,
+
+    created_at DATETIME(6) NULL,
+    updated_at DATETIME(6) NULL,
+
+    CONSTRAINT pk_courses PRIMARY KEY (id),
+
+    INDEX idx_courses_creator_id (creator_id)
+);
+
+CREATE TABLE enrollments (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    course_id BIGINT NOT NULL,
+    member_id BIGINT NOT NULL,
+    status VARCHAR(20) NOT NULL,
+
+    created_at DATETIME(6) NULL,
+    updated_at DATETIME(6) NULL,
+
+    CONSTRAINT pk_enrollments
+        PRIMARY KEY (id),
+
+    CONSTRAINT uk_enrollments_course_member
+        UNIQUE (course_id, member_id),
+
+    INDEX idx_enrollments_member_id (member_id),
+    INDEX idx_enrollments_course_status (course_id, status),
+    INDEX idx_enrollments_course_id (course_id)
+);
