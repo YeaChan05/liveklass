ed820fa0146e901e7b58e8e8b841ad7e7c07231c
qkenrdl05 <qkenrdl05@gmail.com>
2026-05-08 16:13:57 +0900

    chore(course): 불필요한 Spring Security Core 의존성 제거

--- course/service/build.gradle.kts
+++ course/service/build.gradle.kts
@@ -4,6 +4,5 @@ dependencies {
     implementation(project(":common:security"))
     implementation(project(":course:infrastructure"))
     implementation(project(":course:exception"))
-    implementation("org.springframework.security:spring-security-core")
     implementation("org.springframework:spring-tx")
 }
