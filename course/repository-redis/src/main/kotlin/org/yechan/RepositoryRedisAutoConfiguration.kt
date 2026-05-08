9f85578915e0212d8592b2ee8333a0f9cf36aa71
qkenrdl05 <qkenrdl05@gmail.com>
2026-05-08 14:41:29 +0900

    feat(course): 액세스 토큰 블랙리스트 Redis 자동 구성 추가

--- course/repository-redis/src/main/kotlin/org/yechan/RepositoryRedisAutoConfiguration.kt
+++ course/repository-redis/src/main/kotlin/org/yechan/RepositoryRedisAutoConfiguration.kt
@@ -2,6 +2,7 @@ package org.yechan
 
 import org.springframework.beans.factory.BeanRegistrarDsl
 import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.yechan.member.AccessTokenBlacklistRedisRepository
 import org.yechan.member.RefreshTokenRedisRepository
 import org.yechan.member.RefreshTokenRepository
 
@@ -10,5 +11,8 @@ class RepositoryRedisAutoConfiguration :
     BeanRegistrarDsl({
         registerBean<RefreshTokenRepository> {
             RefreshTokenRedisRepository(bean())
+        }
+        registerBean<AccessTokenBlacklistRedisRepository> {
+            AccessTokenBlacklistRedisRepository(bean())
         }
     })
