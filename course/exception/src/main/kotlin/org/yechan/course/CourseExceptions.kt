65d4217d0ebeee4ebb26936cf329df4f3b3243aa
qkenrdl05 <qkenrdl05@gmail.com>
2026-05-08 11:23:00 +0900

    feat(course): 강의 관련 예외 클래스 추가

---
+++ course/exception/src/main/kotlin/org/yechan/course/CourseExceptions.kt
@@ -0,0 +1,35 @@
+package org.yechan.course
+
+import org.yechan.BusinessException
+import org.yechan.Status
+
+class CourseNotFoundException :
+    BusinessException(
+        Status.RESOURCE_NOT_FOUND,
+        "강의를 찾을 수 없습니다.",
+    )
+
+class EnrollmentNotFoundException :
+    BusinessException(
+        Status.RESOURCE_NOT_FOUND,
+        "수강 신청을 찾을 수 없습니다.",
+    )
+
+class CourseAccessDeniedException :
+    BusinessException(
+        Status.FORBIDDEN,
+        "강의를 변경할 권한이 없습니다.",
+    )
+
+class EnrollmentAccessDeniedException :
+    BusinessException(
+        Status.FORBIDDEN,
+        "수강 신청을 변경할 권한이 없습니다.",
+    )
+
+class CourseInvalidStateException(
+    message: String,
+) : BusinessException(
+    Status.BAD_REQUEST,
+    message,
+)
