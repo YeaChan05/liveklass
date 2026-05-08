39a022f37aa94b7e0c6953880aaaccabd88bb69c
qkenrdl05 <qkenrdl05@gmail.com>
2026-05-08 11:24:29 +0900

    feat(course): 리프레시 토큰 Redis 자동 구성 추가

---
+++ course/repository-redis/src/main/kotlin/org/yechan/RepositoryRedisAutoConfiguration.kt
@@ -0,0 +1,14 @@
+package org.yechan
+
+import org.springframework.beans.factory.BeanRegistrarDsl
+import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.yechan.member.RefreshTokenRedisRepository
+import org.yechan.member.RefreshTokenRepository
+
+@AutoConfiguration
+class RepositoryRedisAutoConfiguration :
+    BeanRegistrarDsl({
+        registerBean<RefreshTokenRepository> {
+            RefreshTokenRedisRepository(bean())
+        }
+    })
