2ff6b6cc8d8e1c0e4f383fc89eedb6cc4df9030f
qkenrdl05 <qkenrdl05@gmail.com>
2026-05-09 16:46:31 +0900

    chore(dependencies): 의존 제약에 따른 의존 설정 수정

--- common/security/build.gradle.kts
+++ common/security/build.gradle.kts
@@ -2,7 +2,6 @@ dependencies {
     api("org.springframework.boot:spring-boot-starter-security")
     implementation(project(":common:api"))
     implementation(project(":common:exception"))
-    testImplementation(project(":course:api"))
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation(rootProject.libs.jjwt.api)
     testImplementation(rootProject.libs.jjwt.impl)
