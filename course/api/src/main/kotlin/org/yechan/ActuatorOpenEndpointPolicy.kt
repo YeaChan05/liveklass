3b5cca8b770144d126f1eedcf0ff9d11e1987277
qkenrdl05 <qkenrdl05@gmail.com>
2026-05-10 13:58:12 +0900

    feat(observability): 가시성 노출을 위한 보안 허용 범위 추가 설정

---
+++ course/api/src/main/kotlin/org/yechan/ActuatorOpenEndpointPolicy.kt
@@ -0,0 +1,28 @@
+package org.yechan
+
+import org.springframework.beans.factory.BeanRegistrarDsl
+import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.springframework.core.Ordered
+import org.springframework.http.HttpMethod
+
+@AutoConfiguration
+class ActuatorOpenEndpointPolicy :
+    BeanRegistrarDsl({
+        registerBean<AuthorizeHttpRequestsCustomizer> {
+            PrioritizedAuthorizeHttpRequestsCustomizer(
+                Ordered.HIGHEST_PRECEDENCE,
+                ApplicationOpenEndpointsAuthorizeHttpRequestsCustomizer(
+                    actuatorOpenEndpointPolicy(),
+                ),
+            )
+        }
+    })
+
+private fun actuatorOpenEndpointPolicy(): StaticApplicationOpenEndpointPolicy = StaticApplicationOpenEndpointPolicy(
+    additionalMatchers =
+    listOf(
+        OpenEndpointMatcher(HttpMethod.GET, "/actuator/health"),
+        OpenEndpointMatcher(HttpMethod.GET, "/actuator/info"),
+        OpenEndpointMatcher(HttpMethod.GET, "/actuator/prometheus"),
+    ),
+)
