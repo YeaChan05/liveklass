7a70ffe115c7e0d998a2315bf052328b60243e15
qkenrdl05 <qkenrdl05@gmail.com>
2026-05-10 21:07:43 +0900

    feat(application): Clock 빈 추가

--- course/application/src/main/kotlin/org/yechan/Application.kt
+++ course/application/src/main/kotlin/org/yechan/Application.kt
@@ -2,7 +2,9 @@ package org.yechan
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.time.Clock
 
 @SpringBootApplication
 @EnableScheduling
@@ -11,3 +13,6 @@ class Application
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
 }
+
+@Bean
+fun clock(): Clock = Clock.systemDefaultZone()
