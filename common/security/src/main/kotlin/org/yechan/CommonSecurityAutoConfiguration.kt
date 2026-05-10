147e98199265fba0d8e8801838de4773b7514762
qkenrdl05 <qkenrdl05@gmail.com>
2026-05-11 00:26:41 +0900

    perf(security): 보안 필터 체인 커스터마이징 기능 추가

--- common/security/src/main/kotlin/org/yechan/CommonSecurityAutoConfiguration.kt
+++ common/security/src/main/kotlin/org/yechan/CommonSecurityAutoConfiguration.kt
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.BeanRegistrarDsl
 import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration
 import org.springframework.boot.security.autoconfigure.web.servlet.ServletWebSecurityAutoConfiguration
-import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Import
 import org.springframework.core.Ordered
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -14,9 +14,6 @@ import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.access.AccessDeniedHandler
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-
-class CommonSecurityAutoConfiguration
 
 @AutoConfiguration(
     before = [
@@ -24,8 +21,19 @@ class CommonSecurityAutoConfiguration
         ServletWebSecurityAutoConfiguration::class,
     ],
 )
+@Import(CommonSecurityBeanRegistrar::class)
+class CommonSecurityAutoConfiguration
+
+fun interface SecurityFilterChainCustomizer {
+    fun customize(http: HttpSecurity)
+}
+
 class CommonSecurityBeanRegistrar :
     BeanRegistrarDsl({
+        registerBean<AccessTokenBlacklist>(fallback = true) {
+            NoOpAccessTokenBlacklist
+        }
+
         registerBean<AuthenticationEntryPoint> {
             DefaultAuthenticationEntryPoint()
         }
@@ -34,56 +42,55 @@ class CommonSecurityBeanRegistrar :
             DefaultAccessDeniedHandler()
         }
 
-        registerBean<JwtAuthenticationFilter> {
-            JwtAuthenticationFilter(
-                beanProvider<TokenParser>().ifAvailable ?: NoOpTokenParser,
-                beanProvider<TokenVerifier>().ifAvailable ?: NoOpTokenVerifier,
-                beanProvider<AccessTokenBlacklist>().ifAvailable ?: NoOpAccessTokenBlacklist,
-                bean(),
-            )
-        }
-
-        registerBean<FilterRegistrationBean<JwtAuthenticationFilter>> {
-            FilterRegistrationBean(bean<JwtAuthenticationFilter>()).apply {
-                isEnabled = false
-            }
-        }
-
         registerBean<AuthorizeHttpRequestsCustomizer> {
             PrioritizedAuthorizeHttpRequestsCustomizer(
                 Ordered.LOWEST_PRECEDENCE,
-            ) { registry -> registry.anyRequest().authenticated() }
+            ) { registry ->
+                registry.anyRequest().authenticated()
+            }
         }
 
         registerBean<SecurityFilterChain> {
-            val authorizeHttpRequestsCustomizers = beanProvider<AuthorizeHttpRequestsCustomizer>()
-            val roleHierarchy = beanProvider<RoleHierarchy>().ifAvailable
+            val authorizeHttpRequestsCustomizers =
+                beanProvider<AuthorizeHttpRequestsCustomizer>()
 
-            bean<HttpSecurity>()
-                .formLogin(FormLoginConfigurer<HttpSecurity>::disable)
-                .csrf(CsrfConfigurer<HttpSecurity>::disable)
-                .sessionManagement { session ->
-                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                }
-                .exceptionHandling { handler ->
-                    handler.authenticationEntryPoint(bean())
-                    handler.accessDeniedHandler(bean())
-                }
-                .authorizeHttpRequests { registry ->
-                    authorizeHttpRequestsCustomizers.orderedStream().forEach { customizer ->
-                        customizer.customize(registry)
+            val securityFilterChainCustomizers =
+                beanProvider<SecurityFilterChainCustomizer>()
+
+            val roleHierarchy =
+                beanProvider<RoleHierarchy>().ifAvailable
+
+            val http =
+                bean<HttpSecurity>()
+                    .formLogin(FormLoginConfigurer<HttpSecurity>::disable)
+                    .csrf(CsrfConfigurer<HttpSecurity>::disable)
+                    .sessionManagement { session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                     }
-                }
-                .let { http ->
-                    if (roleHierarchy != null) {
-                        http.setSharedObject(RoleHierarchy::class.java, roleHierarchy)
+                    .exceptionHandling { handler ->
+                        handler.authenticationEntryPoint(bean())
+                        handler.accessDeniedHandler(bean())
                     }
-                    http
+                    .authorizeHttpRequests { registry ->
+                        authorizeHttpRequestsCustomizers
+                            .orderedStream()
+                            .forEach { customizer ->
+                                customizer.customize(registry)
+                            }
+                    }
+                    .let { http ->
+                        if (roleHierarchy != null) {
+                            http.setSharedObject(RoleHierarchy::class.java, roleHierarchy)
+                        }
+                        http
+                    }
+
+            securityFilterChainCustomizers
+                .orderedStream()
+                .forEach { customizer ->
+                    customizer.customize(http)
                 }
-                .addFilterBefore(
-                    bean<JwtAuthenticationFilter>(),
-                    UsernamePasswordAuthenticationFilter::class.java,
-                )
-                .build()
+
+            http.build()
         }
     })
--- common/security/src/main/kotlin/org/yechan/TokenParser.kt
+++ common/security/src/main/kotlin/org/yechan/TokenParser.kt
@@ -5,7 +5,3 @@ import jakarta.servlet.http.HttpServletRequest
 fun interface TokenParser {
     fun parse(request: HttpServletRequest): String?
 }
-
-object NoOpTokenParser : TokenParser {
-    override fun parse(request: HttpServletRequest): String? = null
-}
