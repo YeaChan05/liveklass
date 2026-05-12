f8fc4e8feda217ae9906def54d6d0aaa902d40c4
qkenrdl05 <qkenrdl05@gmail.com>
2026-05-12 10:55:21 +0900

    test(converter): ApiRes현ponseHttpMessageConverter 테스트 추가

--- common/api/build.gradle.kts
+++ common/api/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
     implementation(project(":common:exception"))
     implementation("tools.jackson.module:jackson-module-kotlin")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 }
