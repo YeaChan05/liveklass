9e15ac71d3c6a342ca17f39ff7aa105e06792f1b
qkenrdl05 <qkenrdl05@gmail.com>
2026-05-13 14:02:55 +0900

    feat(enrollment): 대량 수강 신청 및 좌석 해제 기능 추가

--- course/infrastructure/src/main/kotlin/org/yechan/enrollment/CourseBulkWriter.kt
+++ course/infrastructure/src/main/kotlin/org/yechan/enrollment/CourseBulkWriter.kt
@@ -2,4 +2,6 @@ package org.yechan.enrollment
 
 interface CourseBulkWriter {
     fun reserveSeatsBulk(courseIds: Map<Long, Int>)
+
+    fun releaseSeatsBulk(courseIds: Map<Long, Int>)
 }
--- course/infrastructure/src/main/kotlin/org/yechan/enrollment/EnrollmentBulkWriter.kt
+++ course/infrastructure/src/main/kotlin/org/yechan/enrollment/EnrollmentBulkWriter.kt
@@ -1,5 +1,9 @@
 package org.yechan.enrollment
 
+import java.time.LocalDateTime
+
 interface EnrollmentBulkWriter {
     fun saveAllBulk(enrollments: List<EnrollmentModelData>)
+
+    fun updateAllExpired(courseIds: Collection<Long>, now: LocalDateTime): Map<Long, Int>
 }
