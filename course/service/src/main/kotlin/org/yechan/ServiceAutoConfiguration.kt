34345465ab5942c921057f032fdd99855a91e144
qkenrdl05 <qkenrdl05@gmail.com>
2026-05-17 17:47:15 +0900

    refactor(enrollment): 대기열 및 회원 관련 책임을 분리하여 구조 개선

--- course/service/src/main/kotlin/org/yechan/ServiceAutoConfiguration.kt
+++ course/service/src/main/kotlin/org/yechan/ServiceAutoConfiguration.kt
@@ -4,20 +4,28 @@ import org.springframework.beans.factory.BeanRegistrarDsl
 import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Import
+import org.yechan.course.CourseCommandProcessor
+import org.yechan.course.CourseQueryProcessor
 import org.yechan.course.CourseService
 import org.yechan.course.CourseUseCase
 import org.yechan.enrollment.EnrollmentExpirationProcessor
 import org.yechan.enrollment.EnrollmentExpirationService
 import org.yechan.enrollment.EnrollmentPaymentExpirationScheduler
+import org.yechan.enrollment.EnrollmentPaymentExpirationService
 import org.yechan.enrollment.EnrollmentPaymentPendingProperties
 import org.yechan.enrollment.EnrollmentService
 import org.yechan.enrollment.EnrollmentTransactionService
 import org.yechan.enrollment.EnrollmentUseCase
+import org.yechan.enrollment.EnrollmentWaitlistCoordinator
 import org.yechan.enrollment.EnrollmentWaitlistProcessor
 import org.yechan.enrollment.EnrollmentWaitlistPromotionService
 import org.yechan.enrollment.EnrollmentWaitlistScheduler
+import org.yechan.enrollment.WaitlistPromotionRecoveryService
 import org.yechan.member.MemberAuthService
 import org.yechan.member.MemberAuthUseCase
+import org.yechan.member.MemberCurrentMemberProcessor
+import org.yechan.member.MemberRegistrationProcessor
+import org.yechan.member.MemberSessionProcessor
 import java.time.Clock
 
 @Import(ServiceBeanRegistrar::class)
@@ -28,10 +36,19 @@ class ServiceAutoConfiguration
 @AutoConfiguration
 class ServiceBeanRegistrar :
     BeanRegistrarDsl({
+        registerBean<CourseQueryProcessor> {
+            CourseQueryProcessor(bean())
+        }
+        registerBean<CourseCommandProcessor> {
+            CourseCommandProcessor(
+                bean(),
+                bean(),
+            )
+        }
         registerBean<CourseUseCase> {
             CourseService(
-                bean(),
-                bean(),
+                bean<CourseQueryProcessor>(),
+                bean<CourseCommandProcessor>(),
             )
         }
 
@@ -56,33 +73,65 @@ class ServiceBeanRegistrar :
                 bean<EnrollmentPaymentPendingProperties>().expiresIn,
             )
         }
+        registerBean<EnrollmentWaitlistCoordinator> {
+            EnrollmentWaitlistCoordinator(
+                bean(),
+                bean(),
+            )
+        }
+        registerBean<WaitlistPromotionRecoveryService> {
+            WaitlistPromotionRecoveryService(
+                bean(),
+                bean(),
+                bean(),
+            )
+        }
         registerBean<EnrollmentWaitlistScheduler> {
             EnrollmentWaitlistScheduler(
+                bean<WaitlistPromotionRecoveryService>(),
+            )
+        }
+
+        registerBean<MemberRegistrationProcessor> {
+            MemberRegistrationProcessor(
+                bean(),
+                bean(),
+            )
+        }
+        registerBean<MemberSessionProcessor> {
+            MemberSessionProcessor(
+                bean(),
+                bean(),
+                bean(),
+                bean(),
+                bean(),
                 bean(),
                 bean(),
                 bean(),
             )
         }
-
+        registerBean<MemberCurrentMemberProcessor> {
+            MemberCurrentMemberProcessor(bean())
+        }
         registerBean<MemberAuthUseCase> {
             MemberAuthService(
-                bean(),
-                bean(),
-                bean(),
-                bean(),
+                bean<MemberRegistrationProcessor>(),
+                bean<MemberSessionProcessor>(),
+                bean<MemberCurrentMemberProcessor>(),
+            )
+        }
+
+        registerBean<EnrollmentPaymentExpirationService> {
+            EnrollmentPaymentExpirationService(
                 bean(),
                 bean(),
                 bean(),
                 bean(),
             )
         }
-
         registerBean<EnrollmentPaymentExpirationScheduler> {
             EnrollmentPaymentExpirationScheduler(
-                bean(),
-                bean(),
-                bean(),
-                bean(),
+                bean<EnrollmentPaymentExpirationService>(),
             )
         }
 
