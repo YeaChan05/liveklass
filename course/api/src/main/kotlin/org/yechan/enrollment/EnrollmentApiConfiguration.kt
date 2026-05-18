1d706977afec7011b0023c3325e941ac930d6ca2
qkenrdl05 <qkenrdl05@gmail.com>
2026-05-18 11:48:44 +0900

    refactor(enrollment): 수강 신청 API 클래스 이름 변경

--- course/api/src/main/kotlin/org/yechan/enrollment/EnrollmentApiConfiguration.kt
+++ course/api/src/main/kotlin/org/yechan/enrollment/EnrollmentApiConfiguration.kt
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.BeanRegistrarDsl
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class EnrollmentApiAutoConfiguration :
+class EnrollmentApiConfiguration :
     BeanRegistrarDsl({
         registerBean<EnrollmentWaitlistSseHandler> {
             EnrollmentWaitlistSseHandler(
